6800415a2db26d441577d3f87a7d8631050a72f1
Andrew Henry <andrewmhenry@me.com>
2022-01-11 17:38:16 +0800

    🚀 feat(Initial Config): initial nextjs config and contract

---
+++ contracts/Keyboards.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+contract Keyboards {
+  string[] public createdKeyboards;
+
+ // Function to return ALL the keyboards
+  function getKeyboards() view public returns(string[] memory) {
+    return createdKeyboards;
+  }
+
+  function create(string calldata _description) external {
+    createdKeyboards.push(_description);
+  }
+
+}
